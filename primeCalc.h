8b597f5e086d83e93b2083896a1eb6d2102520af
Riley <lannon.riley@gmail.com>
2018-03-15 16:24:49 -0500

    Separated functions into headers; got it working

--- primeCalc.h
+++ primeCalc.h
@@ -1,6 +1,9 @@
 /* primeCalc.h -- our first prime calculation algorithm */
 
-#ifndef _PRIMECALC_H_
+#ifndef PRIMECALC_H
+#define PRIMECALC_H
+
+#include <stdbool.h>
 
 void primeCalc(int dividend, int min, int max, bool print, bool log, bool log_p_num);
 bool calcMethod(int dividend, int& count, int& factor);
